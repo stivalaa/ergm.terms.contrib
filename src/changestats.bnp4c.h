892b152b02cfa16494627e2f6cc14135c4b806a9
Alex Stivala <alex.d.stivala@gmail.com>
2024-12-09 15:46:01 +1100

    new changestats starting as copy of gwbnsp

---
+++ src/changestats.bnp4c.h
@@ -0,0 +1,14 @@
+#ifndef CHANGESTATS_H
+#define CHANGESTATS_H
+
+#include "ergm_edgetree.h"
+#include "ergm_changestat.h"
+
+
+
+/* Add your function names here */
+
+CHANGESTAT_FN(d_b1np4c);
+CHANGESTAT_FN(d_b2np4c);
+
+#endif
