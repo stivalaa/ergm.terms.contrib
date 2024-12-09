8256221d1644f043fa1c78cad22e2f62d8726670
Alex Stivala <alex.d.stivala@gmail.com>
2024-12-10 08:23:10 +1100

    empty new change stats funcs ready for implementation, added documentation

--- src/changestats.bnp4c.c
+++ src/changestats.bnp4c.c
@@ -6,179 +6,48 @@
  */
 #include "changestats.gwbnsp.h"
 
-D_CHANGESTAT_FN(d_b1np4c) { 
-  Edge e, f;
-  int i, echange, ochange;
-  int L2th, L2tu, L2uh;
-  Vertex tail, head, u, v;
-  double alpha, oneexpa, cumchange;
-  
-  CHANGE_STAT[0] = 0.0;
+/*
+ * Change statistics for number of 4-cycles at each node raised to a
+ * power alpha (0 < alpha <= 1) before summing (i.e. the "alpha-inside"
+ * weighting according to the description of Wilson et al. (2017).
+ *
+ * b1np4c is for nodes in the first bipartition, and b2np4c is for nodes
+ * in the second bipartition.
+ *
+ * These statistics are described in Stivala et al. (2024).  The
+ * b1np4c and b2np4c statistics were first implemented in
+ * EstimNetDirected (https://github.com/stivalaa/EstimNetDirected}
+ * as BipartiteFourCyclesNodePowerA and BipartiteFourCyclesNodePowerB.
+ *
+ * Rereferences:
+ *
+ * Stivala, A., Wang., P., and Lomi, A. (2024). Improving
+ * exponential-family random graph models for bipartite
+ * networks. Unpublished manusript.
+ *
+ * Wilson, J. D., Denny, M. J., Bhamidi, S., Cranmer, S. J., &
+ * Desmarais, B. A. (2017). Stochastic weighted graphs: Flexible model
+ * specification and simulation. Social Networks, 49, 37-47.
+ */
 
-  alpha = INPUT_PARAM[0];
-  oneexpa = 1.0-exp(-alpha);
+D_CHANGESTAT_FN(d_b1np4c) {
+  int i;
   
-  /* *** don't forget tail -> head */    
-  FOR_EACH_TOGGLE(i){      
-    cumchange=0.0;
-    ochange = IS_OUTEDGE(tail=TAIL(i), head=HEAD(i)) ? -1 : 0;
-    echange = 2*ochange + 1;
-    STEP_THROUGH_INEDGES(head, e, u){
-      if (u != tail){
-        L2tu=ochange;
-        /* step through outedges of u */
-        STEP_THROUGH_OUTEDGES(u, f, v){
-          if(IS_UNDIRECTED_EDGE(v, tail)) L2tu++;
-        }
-        /* step through inedges of u 
-        STEP_THROUGH_INEDGES(u, f, v){
-          if(IS_UNDIRECTED_EDGE(v, tail)) L2tu++;
-        } */
-        cumchange += pow(oneexpa,(double)L2tu);
-      }
-    }
-    
-    STEP_THROUGH_INEDGES(tail, e, u){
-      if (u != head){
-        L2uh=ochange;
-        /* step through outedges of u */
-        STEP_THROUGH_OUTEDGES(u, f, v){
-          if(IS_UNDIRECTED_EDGE(v, head)) L2uh++;
-        }
-        /* step through inedges of u 
-        STEP_THROUGH_INEDGES(u, f, v){
-          if(IS_UNDIRECTED_EDGE(v, head)) L2uh++;
-        }*/
-        cumchange += pow(oneexpa,(double)L2uh);
-      }
-    }
-    
-    cumchange  = echange*cumchange;
-    (CHANGE_STAT[0]) += cumchange;
+  ZERO_ALL_CHANGESTATS(i);
+  FOR_EACH_TOGGLE(i) {
     TOGGLE_IF_MORE_TO_COME(i);
   }
-  
   UNDO_PREVIOUS_TOGGLES(i);
-
-  alpha = INPUT_PARAM[0];
-  oneexpa = 1.0-exp(-alpha);
-  
-  /* *** don't forget tail -> head */    
-  FOR_EACH_TOGGLE(i){
-    cumchange=0.0;
-    L2th=0;
-    ochange = IS_OUTEDGE(tail=TAIL(i), head=HEAD(i)) ? -1 : 0;
-    echange = 2*ochange + 1;
-
-    STEP_THROUGH_INEDGES(head, e, u){
-      if (IS_UNDIRECTED_EDGE(u, tail)){
-	L2th++;
-	L2tu=ochange;
-	L2uh=ochange;
-	/* step through outedges of u */
-	STEP_THROUGH_OUTEDGES(u, f, v){
-	  if(IS_UNDIRECTED_EDGE(v, head)) L2uh++;
-	  if(IS_UNDIRECTED_EDGE(v, tail)) L2tu++;
-	}
-	cumchange += pow(oneexpa,(double)L2tu) +
-	  pow(oneexpa,(double)L2uh) ;
-      }
-    }
-    
-    if(alpha < 100.0){
-      cumchange += exp(alpha)*(1.0-pow(oneexpa,(double)L2th)) ;
-    }else{
-      cumchange += (double)L2th;
-    }
-    cumchange  = echange*cumchange;
-    (CHANGE_STAT[0]) -= cumchange;
-    TOGGLE_IF_MORE_TO_COME(i);
-  }
-  
-  UNDO_PREVIOUS_TOGGLES(i);
-
 }
 
+
+
 D_CHANGESTAT_FN(d_b2np4c) { 
-  Edge e, f;
-  int i, echange, ochange;
-  int L2th, L2tu, L2uh;
-  Vertex tail, head, u, v;
-  double alpha, oneexpa, cumchange;
+  int i;
   
-  CHANGE_STAT[0] = 0.0;
-
-  alpha = INPUT_PARAM[0];
-  oneexpa = 1.0-exp(-alpha);
-  
-  /* *** don't forget tail -> head */    
-  FOR_EACH_TOGGLE(i){      
-    cumchange=0.0;
-    ochange = IS_OUTEDGE(tail=TAIL(i), head=HEAD(i)) ? -1 : 0;
-    echange = 2*ochange + 1;
-    /* step through outedges of head */
-    STEP_THROUGH_OUTEDGES(head, e, u){
-      if (u != tail){
-        L2tu=ochange;
-        STEP_THROUGH_INEDGES(u, f, v){
-          if(IS_UNDIRECTED_EDGE(v, tail)) L2tu++;
-        }
-        cumchange += pow(oneexpa,(double)L2tu);
-      }
-    }
-    STEP_THROUGH_OUTEDGES(tail, e, u){
-      if (u != head){
-        L2uh=ochange;
-        STEP_THROUGH_INEDGES(u, f, v){
-          if(IS_UNDIRECTED_EDGE(v, head)) L2uh++;
-        }
-        cumchange += pow(oneexpa,(double)L2uh);
-      }
-    }
-
-    cumchange  = echange*cumchange;
-    (CHANGE_STAT[0]) += cumchange;
+  ZERO_ALL_CHANGESTATS(i);
+  FOR_EACH_TOGGLE(i) {
     TOGGLE_IF_MORE_TO_COME(i);
   }
-  
   UNDO_PREVIOUS_TOGGLES(i);
-
-  alpha = INPUT_PARAM[0];
-  oneexpa = 1.0-exp(-alpha);
-  
-  /* *** don't forget tail -> head */    
-  FOR_EACH_TOGGLE(i){
-    cumchange=0.0;
-    L2th=0;
-    ochange = IS_OUTEDGE(tail=TAIL(i), head=HEAD(i)) ? -1 : 0;
-    echange = 2*ochange + 1;
-    /* step through outedges of head  */
-    STEP_THROUGH_OUTEDGES(head, e, u){
-      if (IS_UNDIRECTED_EDGE(u, tail)){
-  L2th++;
-	L2tu=ochange;
-	L2uh=ochange;
-
-	STEP_THROUGH_INEDGES(u, f, v){
-	  if(IS_UNDIRECTED_EDGE(v, head)) L2uh++;
-	  if(IS_UNDIRECTED_EDGE(v, tail)) L2tu++;
-	}
-	cumchange += pow(oneexpa,(double)L2tu) +
-	  pow(oneexpa,(double)L2uh) ;
-      }
-    }
-
-    
-    if(alpha < 100.0){
-      cumchange += exp(alpha)*(1.0-pow(oneexpa,(double)L2th)) ;
-    }else{
-      cumchange += (double)L2th;
-    }
-    cumchange  = echange*cumchange;
-    (CHANGE_STAT[0]) -= cumchange;
-    TOGGLE_IF_MORE_TO_COME(i);
-  }
-  
-  UNDO_PREVIOUS_TOGGLES(i);
-
 }
