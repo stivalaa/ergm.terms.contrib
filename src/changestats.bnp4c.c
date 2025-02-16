6cd24e9f75e611c3317012f6f0dbb59e95d80183
Alex Stivala <alex.d.stivala@gmail.com>
2025-02-17 10:15:00 +1100

    tried with u_ function but assertion fails with NOT_SET as actually teh u_ function is caleld AFTER the c_ function (see API doc) so cannot memoize this way: need to change to set all values in i_ function instead (so no longer need NOT_SET and can change back to unsigned long)

--- src/changestats.bnp4c.c
+++ src/changestats.bnp4c.c
@@ -54,14 +54,18 @@ const long NOT_SET = -1; /* value for fourcycle_count not set */
 /* These are just allocated as arrays of length N_NODES for simplicity;
  * they could perhaps more appropriately be hash tables, but, unlike the case
  * for dyadic properties and so would require N^2 storage, it is not
- * too inefficient to require N storage here. */
+ * too inefficient to require N storage here.
+ * Note than indexiing in these two arrays is 0..N_NODES-1 so
+ * must subrtact one when indexigin from a Vertex which
+ * is 1..N_NODES (R style) */
 typedef struct bnp4c_storage_s {
   int            *visited;         /* visited flag for each node
                                       (working storage) */
   long           *fourcycle_count; /* number of four-cycles at each node
                                     (memoization cache) or NOT_SET.
                                     This is signed (not unsigned) as 
-                                    NOT_SET is -1*/
+                                    NOT_SET is -1 */
+
 } bnp4c_storage_t;
 
 
@@ -131,8 +135,8 @@ static unsigned int twopaths(Network *nwp, Vertex i, Vertex j)  {
 /*
  * Number of four-cycles that node unode is involved in/
  *
- * The bnp4c_storage_t sto parameter containing the visited working storage and
- * the memoization cache for the result is allocated by the caller.
+ * The bnp4c_storage_t sto parameter containing the visited working
+ * storage is allocated by the caller.
  *
  * The return value is signed (not unsigned) to avoid problems with
  * signed/unsigned in the memoization which is signed in order to use
@@ -149,20 +153,7 @@ static long num_fourcycles_node(Network *nwp, Vertex unode,
   long fourcycle_count = 0;
   int *visited = sto->visited;
   
-  /*
-    Note it seems that the Vertex is an int from 1 .. N_NODES
-     (R indexing from 1 rather than C indexing from 0)
-     so we subtract one when indexing the visited and fourcycle_count arrays.
-     This code depends on this being the case.
-  */
-
-  /* memoization: return cached value if set */
-  if (sto->fourcycle_count[unode-1] != NOT_SET) {
-    return sto->fourcycle_count[unode-1];
-  }
-  
   memset(visited, 0, N_NODES*sizeof(visited[0]));
-
   
   /* this involves iterating over all nodes that are distance 2 from unode */
   /* In an undirected network, each edge is only stored as (tail, head) where
@@ -196,7 +187,6 @@ static long num_fourcycles_node(Network *nwp, Vertex unode,
       }
     }
   }
-  sto->fourcycle_count[unode-1] = fourcycle_count; /* memoization */
   return fourcycle_count;
 }
 
@@ -205,11 +195,11 @@ static long num_fourcycles_node(Network *nwp, Vertex unode,
  * Change statistic for number of four-cycles in undirected network
  * when edge i -- j is added
  */
-static unsigned long change_fourcycles(Network *nwp, Vertex i, Vertex j) {
+static long change_fourcycles(Network *nwp, Vertex i, Vertex j) {
   /* Note Network *nwp parameter has to be called nwp for use of macros */
   Vertex vnode;
   Edge edge;
-  unsigned long delta = 0;
+  long delta = 0;
 
   /* In an undirected network, each edge is only stored as (tail, head) where
      tail < head, so to step through all edges of a node it is necessary
@@ -251,12 +241,70 @@ I_CHANGESTAT_FN(i_b1np4c) {
   ALLOC_STORAGE(1, bnp4c_storage_t, sto1);
   sto1->visited = R_Calloc(N_NODES, int);
   sto1->fourcycle_count = R_Calloc(N_NODES, long);
+  /* initialize nodewise fourcycle count cache to NOT_SET */
+  for (int i = 0; i < N_NODES; i++) {
+    sto1->fourcycle_count[i] = NOT_SET;
+  }
 }
 
 I_CHANGESTAT_FN(i_b2np4c) {
   ALLOC_STORAGE(1, bnp4c_storage_t, sto2);
   sto2->visited = R_Calloc(N_NODES, int);
   sto2->fourcycle_count = R_Calloc(N_NODES, long);
+  /* initialize nodewise fourcycle count cache to NOT_SET */
+  for (int i = 0; i < N_NODES; i++) {
+    sto2->fourcycle_count[i] = NOT_SET;
+  }
+}
+
+/* Updater: will be called when toggling (tail, head) with state
+ * edgestate is imminent.
+ *
+ * The updater function keeps the private storage up-to-date with
+ * number of four-cycles at each node, which is used in the change
+ * statistics function.
+ *
+ */
+U_CHANGESTAT_FN(u_b1np4c) {
+  long delta;
+  Vertex b1, b2;
+  int is_delete;
+
+  fprintf(stderr, "XXX u_b1np4c entered\n");//XXX
+  error('XXX');
+
+  GET_STORAGE(bnp4c_storage_t, sto1); /* Obtain a pointer to private storage
+                                         and cast it to the correct type. */
+  b1 = tail;
+  b2 = head;
+  is_delete = IS_UNDIRECTED_EDGE(b1, b2);
+
+  /* NOTE: For a delete move, we actually toggle the edge ourselves here so
+   * that the proposed edge is always NOT present for all the calculations
+   * as they involve counting two-paths and four-cycles, on the assumption
+   * that the proposed edge does not (yet) exist. We must therefore
+   * also add it back afterwards to fit in with the standard logic.
+   */
+  if (is_delete) {
+    TOGGLE(b1, b2);
+  }
+  if (IS_UNDIRECTED_EDGE(b1, b2)) error("Edge must not exist\n");
+
+  if (sto1->fourcycle_count[b1-1] == NOT_SET) {
+    fprintf(stderr, "XXX u_b1np4c setting %d\n",b1);//XXX
+    sto1->fourcycle_count[b1-1] = num_fourcycles_node(nwp, b1, sto1);
+  }
+
+  /* change statistic for four-cycles */
+  delta = change_fourcycles(nwp, b1, b2);
+
+  sto1->fourcycle_count[b1-1] += is_delete ? -delta : delta;
+
+  /* For a delete move, we deleted the edge at the start, now add it again */
+  if (is_delete) {
+    TOGGLE(b1, b2);
+    if (!IS_UNDIRECTED_EDGE(b1, b2)) error("Edge must exist\n");
+  }
 }
 
 
@@ -271,11 +319,6 @@ C_CHANGESTAT_FN(c_b1np4c) {
 
   GET_STORAGE(bnp4c_storage_t, sto1); /* Obtain a pointer to private storage
                                        and cast it to the correct type. */
-  /* initialize nodewise fourcycle count cache to NOT_SET */
-  /* initialize nodewise fourcycle count cache to NOT_SET */
-  for (int i = 0; i < N_NODES; i++) {
-    sto1->fourcycle_count[i] = NOT_SET;
-  }
 
   alpha = INPUT_PARAM[0];
 
@@ -294,7 +337,8 @@ C_CHANGESTAT_FN(c_b1np4c) {
   if (IS_UNDIRECTED_EDGE(b1, b2)) error("Edge must not exist\n");
 
   /* Number of four-cycles the node is already involved in */
-  count = num_fourcycles_node(nwp, b1, sto1);
+  count = sto1->fourcycle_count[b1-1];
+  if (count == NOT_SET) error("b1np4c fourcycle count is not set [1]\n");
 
   /* change statistic for four-cycles */
   delta = change_fourcycles(nwp, b1, b2);
@@ -302,7 +346,8 @@ C_CHANGESTAT_FN(c_b1np4c) {
 
   /* add contribution from sum over neighbours of b2 */
   EXEC_THROUGH_EDGES(b2, edge, vnode,  { /* step through edges of b2 */
-    vcount = num_fourcycles_node(nwp, vnode, sto1);
+    vcount = sto1->fourcycle_count[vnode-1];
+    if (vcount == NOT_SET) error("b1np4c fourcycle count is not set [2]\n");
     delta = twopaths(nwp, vnode, b1);
     change += pow(vcount + delta, alpha) - pow(vcount, alpha);
   });
@@ -374,8 +419,8 @@ C_CHANGESTAT_FN(c_b2np4c) {
 /* Finalizer: free private storage. */
 
 /* There is no need to free private storage allocated by ALLOC_STORAGE
-   with a Finalizer (f_) function, it is done by statnet elsewhwere
-   (see point 9 ModelDestroy() is called in the API definition:
+   with a Finalizer (f_) function, it is done by statnet elsewhwere,
+   see point 9 ModelDestroy() is called in the API definition:
    https://cran.r-project.org/web/packages/ergm/vignettes/Terms-API.html
 
    However we do need to free the storage inside this that we allocated
