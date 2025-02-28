8f4d46a296338cd311bc15a9f86177e14b0e0fc4
Alex Stivala <alex.d.stivala@gmail.com>
2025-02-28 13:10:59 +1100

    comment out #ifdef DEBUG inside macro to stop warning

--- src/changestats.bnp4c.c
+++ src/changestats.bnp4c.c
@@ -394,10 +394,10 @@ C_CHANGESTAT_FN(c_b1np4c) {
   EXEC_THROUGH_EDGES(b2, edge, vnode,  { /* step through edges of b2 */
     if (vnode == b1) continue; /* except for b1 -- b2 edge (if is_delete) */
     vcount = sto1->fourcycle_count[vnode-1];
-#ifdef DEBUG
-    /* using #ifdef inside macro (EXEC_THROUGH_EDGES) gives compiler warning */
-    if (num_fourcycles_node(nwp, vnode, sto1) != vcount) error("b1np4c incorrect fourcycle count [2] for %d correct %lu got %lu\n", vnode, num_fourcycles_node(nwp, vnode, sto1), vcount);
-#endif /* DEBUG */
+/* #ifdef DEBUG */
+/*     /\* using #ifdef inside macro (EXEC_THROUGH_EDGES) gives compiler warning *\/ */
+/*     if (num_fourcycles_node(nwp, vnode, sto1) != vcount) error("b1np4c incorrect fourcycle count [2] for %d correct %lu got %lu\n", vnode, num_fourcycles_node(nwp, vnode, sto1), vcount); */
+/* #endif /\* DEBUG *\/ */
     delta = twopaths(nwp, vnode, b1, b1, b2);
     change += is_delete ? pow(vcount, alpha) - pow(vcount - delta, alpha) :
       pow(vcount + delta, alpha) - pow(vcount, alpha);
@@ -442,10 +442,10 @@ C_CHANGESTAT_FN(c_b2np4c) {
   EXEC_THROUGH_EDGES(b1, edge, vnode, { /* step through edges of b1 */
     if (vnode == b2) continue; /* except for b1 -- b2 edge (if is_delete) */
     vcount = sto2->fourcycle_count[vnode-BIPARTITE-1];
-#ifdef DEBUG
-    /* using #ifdef inside macro (EXEC_THROUGH_EDGES) gives compiler warning */
-    if (num_fourcycles_node(nwp, vnode, sto2) != vcount) error("b2np4c incorrect fourcycle count [2] for %d correct %lu got %lu\n", vnode, num_fourcycles_node(nwp, vnode, sto2), vcount);
-#endif /* DEBUG */
+/* #ifdef DEBUG */
+/*     /\* using #ifdef inside macro (EXEC_THROUGH_EDGES) gives compiler warning *\/ */
+/*     if (num_fourcycles_node(nwp, vnode, sto2) != vcount) error("b2np4c incorrect fourcycle count [2] for %d correct %lu got %lu\n", vnode, num_fourcycles_node(nwp, vnode, sto2), vcount); */
+/* #endif /\* DEBUG *\/ */
     delta = twopaths(nwp, vnode, b2, b1, b2);
     change += is_delete ? pow(vcount, alpha) - pow(vcount - delta, alpha) :
       pow(vcount + delta, alpha) - pow(vcount, alpha);
